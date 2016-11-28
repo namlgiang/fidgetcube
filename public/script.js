7ac18b5effd6d6c52533fbeda49615a780b7615a
Giang Nam <namle@kinssolutions.net>
2016-11-28 19:16:16 +0700

    asd

---
+++ public/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $(".color li").click(function() {
+    $(".color li").removeClass("active");
+    $(this).addClass("active");
+    var id=$(this).index(".color li");
+    $(".product").css("background-image", "url(product/"+id+".jpg)");
+    $(".form").removeClass("active");
+    $(".form-" + id).addClass("active");
+    $(".purchase>h2").text($(".form-" + id).attr("pname"));
+  });
+
+  $(".buy").click(function() {
+    $('html, body').animate({
+      scrollTop: $(".product").offset().top - 200
+    }, 1000);
+  });
+});
