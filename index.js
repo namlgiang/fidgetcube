54b6d34f0ed88bd36fc2887be1edb7040d8ad84b
Giang Nam <namle@kinssolutions.net>
2016-11-28 22:00:57 +0700

    dasd

--- index.js
+++ index.js
@@ -1,52 +1,18 @@
 var express = require('express')
 var app = express()
 
-// returns an instance of node-letsencrypt with additional helper methods
-var lex = require('letsencrypt-express').create({
-  // set to https://acme-v01.api.letsencrypt.org/directory in production
+'use strict';
+
+require('letsencrypt-express').create({
+
   server: 'staging'
 
-// If you wish to replace the default plugins, you may do so here
-//
-, challenges: { 'http-01': require('le-challenge-fs').create({ webrootPath: '/tmp/acme-challenges' }) }
-, store: require('le-store-certbot').create({ webrootPath: '/tmp/acme-challenges' })
+, email: 'john.doe@example.com'
 
-// You probably wouldn't need to replace the default sni handler
-// See https://github.com/Daplie/le-sni-auto if you think you do
-//, sni: require('le-sni-auto').create({})
+, agreeTos: true
 
-, approveDomains: approveDomains
-});
+, approveDomains: [ 'example.com' ]
 
-function approveDomains(opts, certs, cb) {
-  // This is where you check your database and associated
-  // email addresses with domains and agreements and such
+, app: app.use(express.static('public'))
 
-
-  // The domains being approved for the first time are listed in opts.domains
-  // Certs being renewed are listed in certs.altnames
-  if (certs) {
-    opts.domains = certs.altnames;
-  }
-  else {
-    opts.email = 'namlgiang@gmail.com';
-    opts.agreeTos = true;
-  }
-
-  // NOTE: you can also change other options such as `challengeType` and `challenge`
-  // opts.challengeType = 'http-01';
-  // opts.challenge = require('le-challenge-fs').create({});
-
-  cb(null, { options: opts, certs: certs });
-}
-
-require('http').createServer(lex.middleware(require('redirect-https')())).listen(80, function () {
-  console.log("Listening for ACME http-01 challenges on", this.address());
-});
-
-app.use(express.static('public'));
-
-// handles your app
-require('https').createServer(lex.httpsOptions, lex.middleware(app)).listen(443, function () {
-  console.log("Listening for ACME tls-sni-01 challenges and serve app on", this.address());
-});
+}).listen(80, 443);
